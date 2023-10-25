c0c02d33b780679b73cd5c449742383fd3ec0a81
user95401 <user95401@gmail.com>
2023-10-25 15:12:41 +0400

    Initial commit

---
+++ geode/about.md
@@ -0,0 +1,12 @@
+# ${PROJECT_NAME}
+
+GD does not load the gradients correctly, this is because the format of the textures is in <cr>RGBA4444</c>, so this mod changes it to <cg>RGBA8888</c>.
+
+
+## <co>Converted for Geode</c>
+---
+
+<cr>This is traditional mod that uses minhook, gd.h, cocos-haders!
+
+BUT! In general, this mod works, and IN MOST CASES IT is COMPATIBLE.
+- Made with curly-eureka</c>
---
+++ src/_main.cpp
@@ -0,0 +1,27 @@
+﻿#include "mod_utils.hpp"
+#include "hooks.hpp"
+using namespace cocos2d;
+using namespace cocos2d::extension;
+using namespace gd;
+using namespace std;
+
+#include "SimpleIni.h"
+
+inline void(__thiscall* GameManager_update)(GameManager*, float);
+void __fastcall GameManager_update_H(GameManager* self, void*, float dt) {
+    GameManager_update(self, dt);
+    CCTexture2D::setDefaultAlphaPixelFormat(CCTexture2DPixelFormat::kCCTexture2DPixelFormat_RGBA8888);
+}
+
+DWORD WINAPI PROCESS_ATTACH(void* hModule) {
+    MH_SafeInitialize();
+    HOOK(base + 0xce440, GameManager_update);
+    CCTexture2D::setDefaultAlphaPixelFormat(CCTexture2DPixelFormat::kCCTexture2DPixelFormat_RGBA8888);
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+        CreateThread(0, 0, PROCESS_ATTACH, hModule, 0, 0);
+    return TRUE;
+}
